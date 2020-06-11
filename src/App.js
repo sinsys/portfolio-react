1ee3296ff269cfadbb7ba7e57657d2357e323990
sinsys <nicholas.hazel@gmail.com>
2020-06-11 09:11:09 -0700

    Added fallback backgrounds

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Button from 'components/elements/Button/Button';
 import './App.scss';
 
 function App() {
   return (
     
     <div className="App">
-      <Button 
-        text="Who am I?" 
-        type="slide"
-      />
+
     </div>
   );
 }
